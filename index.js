6ea4d11ff26e5625670a4f5e57077e93bdd5c116
David Caffrey <dave.caffrey71@gmail.com>
2020-02-01 16:54:20 +0000

    added extra dropdowns and made multiple calls to yahoo finance api for tests

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-var sym = "GOOG";
-var http = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=" + sym;
-var resp = "";
-var jsObj = null;
-
-
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": http,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=NFLX",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
 		"x-rapidapi-key": "801e9cc85emsh011e19eee53d290p16417ejsn0b14e78123b6"
 	}
 }
-
-$.ajax(settings).done(function (response) {
- 
+var resultOfQuery = $.ajax(settings).done(function (response) {
+    // turns JSON Object into a string
     resp = JSON.stringify(response);
     console.log(resp);
+    // turns string into JSON Object
     jsObj = JSON.parse(resp);
     console.log(jsObj);
-    console.log(jsObj.cashflowStatementHistory);
-    console.log(jsObj.price.regularMarketOpen.fmt);
-    console.log(jsObj.price.regularMarketDayHigh.fmt);
+
+    return jsObj;
+    // deep dive through JSON Objects to get data points
+    /*console.log(jsObj.cashflowStatementHistory.cashflowStatements[0].investments.longFmt);
+        not all calls work this call did not work on AMRN so have to pick data points that are common to all financial statments
+
+    */
+    //console.log(jsObj.price.regularMarketOpen.fmt);
+    //console.log(jsObj.price.regularMarketDayHigh.fmt);
 });
+
 
 /*function myFunction() {
   setTimeout(function(){ 

b38c76434e21413417dbbdfcf5db884031660a51
David Caffrey <dave.caffrey71@gmail.com>
2020-02-03 09:46:49 +0000

    early styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var settings = {
+/*var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=NFLX",
@@ -18,19 +18,20 @@ var resultOfQuery = $.ajax(settings).done(function (response) {
 
     return jsObj;
     // deep dive through JSON Objects to get data points
-    /*console.log(jsObj.cashflowStatementHistory.cashflowStatements[0].investments.longFmt);
+    console.log(jsObj.cashflowStatementHistory.cashflowStatements[0].investments.longFmt);
         not all calls work this call did not work on AMRN so have to pick data points that are common to all financial statments
 
-    */
+    
     //console.log(jsObj.price.regularMarketOpen.fmt);
     //console.log(jsObj.price.regularMarketDayHigh.fmt);
 });
 
 
-/*function myFunction() {
+function myFunction() {
   setTimeout(function(){ 
      console.log(jsObj.price.regularMarketOpen.fmt);
      console.log(jsObj.price.regularMarketDayHigh.fmt);
    }, 3000);
-}*/
+};
+*/
 

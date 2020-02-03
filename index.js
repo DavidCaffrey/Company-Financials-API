28f7017cb41537a2ea0a47c00e8b3dd6a07e9ee9
David Caffrey <dave.caffrey71@gmail.com>
2020-02-03 15:25:56 +0000

    got api from user button click

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 
-var jsObj;
-var settings = {
+var str = "AAPL";
+
+
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=FB",
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=" + str,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
 		"x-rapidapi-key": "801e9cc85emsh011e19eee53d290p16417ejsn0b14e78123b6"
 	}
 }
- $.ajax(settings).done(function (response) {
+        $.ajax(settings).done(function (response) {
     // turns JSON Object into a string
-    resp = JSON.stringify(response);
+         resp = JSON.stringify(response);
     //console.log(resp);
     // turns string into JSON Object
-    jsObj = JSON.parse(resp);
-    console.log(jsObj);
+        jsObj = JSON.parse(resp);
+        console.log(jsObj);
 
-    return jsObj;
+   
     /* deep dive through JSON Objects to get data points
     console.log(jsObj.cashflowStatementHistory.cashflowStatements[0].investments.longFmt);
         not all calls work this call did not work on AMRN so have to pick data points that are common to all financial statments
@@ -27,7 +33,12 @@ var settings = {
     //console.log(jsObj.price.regularMarketOpen.fmt);
     //console.log(jsObj.price.regularMarketDayHigh.fmt);
     */
+    });
+    });
 });
+
+
+
 
     
  
